6e3b08143044eeed0e9a4322e6f74cc578f64a6c
kh9504 <k.hiraizumi0417@gmail.com>
2023-02-26 15:32:16 +0900

    add pdfparser

--- phpTest/index.php
+++ phpTest/index.php
@@ -1,2 +1,25 @@
 <?php
-  phpinfo();
+require 'vendor/autoload.php';
+
+$parser    = new \Smalot\PdfParser\Parser();
+$pdf       = $parser->parseFile('pdf/sample.pdf');
+$firstPage = $pdf->getPages()[0];
+
+preg_match_all('/¥(.+)-/', $firstPage->getText(), $match);
+$amount  = $match[1][0];
+
+// $template = <<<EOM
+// 今月の請求になります。よろしくお願いいたします。
+// ```
+// 1)支払先 : 〇〇
+// 2)金　額 : {$amount}円
+// 3)予　算 : 通信費
+// 4)内　容 : 〇〇
+// 5)備　考 : 計上日 〇〇 / 支払期限 〇〇
+// 6)決裁者 : 〇〇
+// ```
+// EOM;
+
+echo nl2br($firstPage->getText());
+
+// echo "<pre>{$template}</pre>";
